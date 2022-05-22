0c99c198f44ea9f6ef97c04bd349a09b39b73f8a
Kasun Dissanayake <kasun.bdn@gmail.com>
2022-05-22 14:20:06 +0530

    Update index.js

--- example/simple/index.js
+++ example/simple/index.js
@@ -22,7 +22,15 @@ async function run () {
 
 await producer.connect()
 await producer.send({
-    topic: 'topic-name'
+    topic: 'topic-name',
+    messages: [{
+        key: 'key1',
+        value: 'hello world',
+        headers: {
+            'correlation-id': '2bfb68bb-893a-423b-a7fa-7b568cad5b67',
+            'system-id': 'my-system',
+        }
+    }]
 })
   await admin.disconnect()
 }
