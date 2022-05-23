53d2ec968e3645c0c0abad1a90ec43624ecca00a
Kasun Dissanayake <kasun.bdn@gmail.com>
2022-05-23 07:01:31 +0530

    Update index.js

--- example/simple/index.js
+++ example/simple/index.js
@@ -39,22 +39,62 @@ setInterval(async () => {
 });
                 }, 500);
   
-  const consumer = kafka.consumer({ groupId: 'test-group'+ Date.now()})
-  await consumer.subscribe({ topic: 'lime',fromBeginning: true})
-    await consumer.run({
-      eachMessage: async ({ topic, message }) => {
-        console.log({
-          value: message.value.toString(),
-          topic
-        })
-      }
-    });
    await producer.send({
     topic: 'lime',
     messages: [{
         value: 'hello world'+ Date.now()
     }]
 });
+  
+  
+  
+  
+  ///////////////////////////////////
+  
+  const topic = 'lime'
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+  await consumer.connect()
+  await consumer.subscribe({ topic, fromBeginning: true })
+  await consumer.run({
+    // eachBatch: async ({ batch }) => {
+    //   console.log(batch)
+    // },
+    eachMessage: async ({ topic, partition, message }) => {
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+      console.log(`- ${prefix} ${message.key}#${message.value}`)
+    },
+  })
+}
+
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.forEach(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.forEach(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
   
   
 }
